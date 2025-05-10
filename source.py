ea50087a87deb3fb46a2278e930cd5f4050050b0
fennecsat <fennecsat@outlook.com>
2025-05-10 16:53:39 +0100

    Update source.py

--- source.py
+++ source.py
@@ -3,7 +3,7 @@ import re
 
 # Config
 URL = "http://livepptv.net/get.php?username=299819323222593&password=1593574628&type=m3u_plus&output=ts"
-GROUP_WHITELIST = ["FRANCE H265", "FRANCE FHD", "FRANCE HD", "FRANCE SD", "SPORT FR", "SPORT AR", "ALGERIA", "ARABIC+", "ARABIC"]  # Change this to your desired groups
+GROUP_WHITELIST = ["FRANCE H265", "FRANCE FHD", "FRANCE HD", "FRANCE SD", "SPORT FR", "SPORT AR", "ALGERIA", "ARABIC+", "ARABIC"] # Change this to your desired groups
 OUTPUT_FILE = "playlist.m3u"
 
 # Download playlist
